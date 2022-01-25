ac07024245b04b710ac551f43c5304b47e79eba3
PCottais <pierre.cottais@etudiant.univ-rennes2.fr>
2022-01-25 09:42:09 +0100

    iSSF for each of the 4 colonies

--- iSSA.R
+++ iSSA.R
@@ -69,9 +69,14 @@ stps <- trks %>%
   amt::filter_min_n_burst(min_n = 3) %>% 
   amt::steps_by_burst(keep_col = 'end')
 
-# deal with duplicate burst_id
-stps %>% as_tibble() %>% group_by(burst_) %>% 
-  summarize(nb_ind = n_distinct(id))
+# # deal with duplicate burst_id
+# stps %>% as_tibble() %>% group_by(burst_) %>% 
+#   summarize(nb_ind = n_distinct(id))
+# 
+# # 1st idea: modify `burst_` values so it is different for each bird
+# stps <- stps %>% 
+#   mutate(burst_ = paste(id, burst_, sep = ''))
+
 
 # observed + random steps
 stps <- stps %>% 
@@ -98,20 +103,76 @@ get_habitat <- function(steps, var = c("Bathy",'SST28','logCHLA28')){
 
 # get covariates of available steps from oceano grid
 start <- Sys.time()
-stps <- stps %>% get_habitat()
+stps[stps$case_==FALSE,] <- stps %>% 
+  filter(case_ == FALSE) %>% 
+  get_habitat()
 end <- Sys.time()
 print(end-start)
 
 
-
-
-
 # iSSA conditional regression model
-mod_G <- stps %>% amt::fit_issf(case_ ~ sl_ + ta_ + log_sl + strata(id, step_id_) +
+mod_G <- stps %>% amt::fit_issf(case_ ~ sl_ + ta_ + log_sl + strata(step_id_) +
                                   Bathy + SST28 + logCHLA28)
 mod_G$model
 
 
 
-
 # PART 3 : iSSA for each colony ----
+
+# function for the whole iSSA
+iSSA <- function(colony = "Giraglia", year = 2011){
+  if(year == 2011){
+    data <- ALL2011
+  }else{
+    dataa <- ALL2012
+  }
+  
+  # select all birds from a colony in 2011 (Scopoli's babies)
+  df <- data %>% 
+    filter(Site == colony) %>% 
+    select(x = Longitude, y = Latitude, t = Time, id = ID,
+           Bathy, SST28, logCHLA28)  # covariates
+  
+  # tracks
+  trks <- df %>% 
+    amt::make_track(.x = x, .y = y, .t = t, crs = "epsg:2154", all_cols = TRUE)
+  
+  # observed steps
+  stps <- trks %>% 
+    amt::track_resample(rate = minutes(5), tolerance = seconds(60)) %>% 
+    amt::filter_min_n_burst(min_n = 3) %>% 
+    amt::steps_by_burst(keep_col = 'end')
+  
+  # deal with duplicate burst_id
+  stps %>% as_tibble() %>% group_by(burst_) %>%
+    summarize(nb_ind = n_distinct(id))
+  # 1st idea: modify `burst_` values so it is different for each bird
+  stps <- stps %>%
+    mutate(burst_ = paste(id, burst_, sep = ''))
+  
+  # observed + random steps
+  stps <- stps %>% 
+    amt::random_steps(n_control = 9) %>% 
+    mutate(log_sl = log(sl_),
+           burst_ = as.factor(burst_),
+           step_id_ = as.factor(step_id_))
+  
+  # get covariates of available steps from oceano grid
+  stps[stps$case_==FALSE,] <- stps %>% 
+    filter(case_ == FALSE) %>% 
+    get_habitat()
+  
+  # iSSA conditional regression model
+  model <- stps %>% amt::fit_issf(case_ ~ sl_ + ta_ + log_sl + strata(step_id_) +
+                                       Bathy + SST28 + logCHLA28)
+  return(model)
+}
+
+# initialisation
+models <- list()
+start <- Sys.time()
+for (site in unique(ALL2011$Site)){
+  models[[site]] <- iSSA(colony = site, year = 2011)
+}
+end <- Sys.time()
+print(end-start)
